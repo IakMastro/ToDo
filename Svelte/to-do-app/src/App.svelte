4acd2bbadadbff8e96382424fefe8dbbaeeb3509
IakMastro <cse242017102@uniwa.gr>
2021-08-22 14:46:36 +0300

    Svelte

---
+++ Svelte/to-do-app/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+import ToDo from './components/ToDo.svelte'
+</script>
+
+<div id="app">
+  <h1>To Do Webapp</h1>
+  <ToDo/>
+</div>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Svelte/to-do-app/src/components/ToDo.svelte
@@ -0,0 +1,21 @@
+<script>
+    let tasks = []
+    let task = ''
+
+    function addTask() {
+        tasks = [...tasks, task]
+    }
+</script>
+
+<ul>
+    {#each tasks as task}
+    <li>{task}</li>
+    {/each}
+</ul>
+
+<div>
+    <label for="task">Thing To Add:</label>
+    <input type="text" name="task" id="task" bind:value="{task}">
+</div>
+
+<button title="Add task" on:click="{addTask}">Add</button>
