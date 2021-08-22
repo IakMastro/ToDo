78dd2d0d2040ed8ddc231e5ff72756b41d40d924
IakMastro <cse242017102@uniwa.gr>
2021-08-22 12:58:03 +0300

    VueJS

---
+++ Vue/to-do-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>To Do Webapp</h1>
+    <to-do></to-do>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import ToDo from "./components/ToDo.vue";
+
+@Options({
+  components: {
+    ToDo,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/to-do-app/src/components/ToDo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <ul>
+            <div v-for="(task, index) in tasks" :key="index">
+                <li>{{task}}</li>
+            </div>
+        </ul>
+
+        <div>
+            <label for="task">Thing To Add:</label>
+            <input type="text" name="task" id="task" v-model="task">
+
+            <button title="Add task" @click="addTask()">Add</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    data() { 
+        return {
+            task: '',
+            tasks: []
+        }
+    },
+    methods: {
+        addTask(): void {
+            this.tasks.push(this.task)
+        }
+    }
+})
+</script>
+
+
+<style scoped>
+
+</style>
